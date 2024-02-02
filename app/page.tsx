54b1c857c2183c6cba786e0136320d5ff35183c2
Beaskyy <afeez.akintobi@yahoo.com>
2024-02-02 21:36:25 +0100

    development branch

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center">
-      <h1 className="text-6xl font-bold text-emerald-400">Kits On Earth</h1>
+    <main className="flex min-h-screen items-center justify-center">
+      <h1 className="text-6xl font-bold text-emerald-400 text-center">Kits On Earth</h1>
     </main>
   );
 }
