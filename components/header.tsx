cc0217b1a6e2c8dcfa0ebccb28c3dd28b5818794
Beaskyy <afeez.akintobi@yahoo.com>
2024-02-07 12:37:23 +0100

    responsivenes of the mobile page

--- components/header.tsx
+++ components/header.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
   return (
     <main className="">
-      <div className="flex justify-between items-center w-full bg-emerald-400 px-40 py-5">
+      <div className="lg:flex hidden justify-between items-center w-full bg-emerald-400 lg:px-40 py-5">
         <div>Kits On Earth</div>
         <div>
             <ul className="flex justify-evenly items-center gap-x-12">
