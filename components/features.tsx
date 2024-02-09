0a95bc90c2880115cd77e6b40b19616033e120bd
Beaskyy <afeez.akintobi@yahoo.com>
2024-02-09 21:36:20 +0100

    features

--- components/features.tsx
+++ components/features.tsx
@@ -3,10 +3,14 @@ import Image from "next/image";
 const Features = () => {
   return (
     <main className="p-4 lg:px-40 py-20">
-      <h2 className="text-3xl lg:text-6xl font-bold mb-10 text-center">How it works</h2>
+      <h2 className="text-3xl lg:text-6xl font-bold mb-10 text-center">
+        How it works
+      </h2>
       <div className="grid grid-cols-1 lg:grid-cols-2">
         <div>
-          <h4 className="text-xl lg:text-3xl font-semibold">Exchange your waste</h4>
+          <h4 className="text-xl lg:text-3xl font-semibold">
+            Exchange your waste
+          </h4>
           <p className="max-w-md mt-4 text-slate-600">
             Collect and sort your recyclable waste - plastic bottles, empty
             cans, unused books, and more.
@@ -36,28 +40,33 @@ const Features = () => {
           </p>
         </div>
         <div>
-          <h4 className="text-xl lg:text-3xl font-semibold">Redeem your rewards</h4>
+          <h4 className="text-xl lg:text-3xl font-semibold">
+            Redeem your rewards
+          </h4>
           <p className="max-w-md mt-4 text-slate-600">
             Choose from a variety of rewards, including:
           </p>
           <ul className="pl-8">
             <li className="list-disc">
-              Food vouchers: Enjoy delicious meals from our partner restaurants.
+              <span className="font-bold">Food vouchers:</span> Enjoy delicious
+              meals from our partner restaurants.
             </li>
             <li className="list-disc">
-              Filling station: Redeem your rewards for fuel at participating
-              stations.
+              <span className="font-bold">Filling station:</span> Redeem your
+              rewards for fuel at participating stations.
             </li>
             <li className="list-disc">
-              Medical assistance: Access basic healthcare services from
-              participating hospitals and pharmacies.
+              <span className="font-bold">Medical assistance:</span> Access
+              basic healthcare services from participating hospitals and
+              pharmacies.
             </li>
             <li className="list-disc">
-              Money: Receive cash payouts for your contributions.
+              <span className="font-bold">Money:</span> Receive cash payouts for
+              your contributions.
             </li>
             <li className="list-disc">
-              App vouchers: Use points to purchase new products directly through
-              the app.
+              <span className="font-bold">App vouchers:</span> Use points to
+              purchase new products directly through the app.
             </li>
           </ul>
         </div>
