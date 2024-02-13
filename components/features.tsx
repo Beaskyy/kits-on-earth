b8d98453df679c1c61ea42c21e2b92563a90f5c0
Beaskyy <afeez.akintobi@yahoo.com>
2024-02-13 21:31:51 +0100

    UI updates

--- components/features.tsx
+++ components/features.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const Features = () => {
   return (
     <main className="p-4 lg:px-40 py-20">
-      <h2 className="text-3xl lg:text-6xl font-bold mb-10 text-center">
+      <h2 className="text-3xl lg:text-6xl font-bold mb-10 text-center mt-10">
         How it works
       </h2>
       <div className="grid grid-cols-1 lg:grid-cols-2">
@@ -28,10 +28,11 @@ const Features = () => {
       <div className="grid grid-cols-1 lg:grid-cols-2 mt-20">
         <div className="flex items-center justify-center gap-10 order-last lg:order-first">
           <Image
-            src="/images/hero-img.png"
-            alt="exchange"
-            width={600}
-            height={600}
+            src="/images/reward.jpg"
+            alt="reward"
+            width={400}
+            height={400}
+            className="rounded-3xl"
           />
         </div>
         <div className="">
@@ -42,7 +43,7 @@ const Features = () => {
           </p>
         </div>
       </div>
-      <div className="grid grid-cols-1 lg:grid-cols-2">
+      <div className="grid grid-cols-1 lg:grid-cols-2 mt-20">
         <div>
           <h4 className="text-xl lg:text-3xl font-semibold">
             Redeem your rewards
--- components/hero.tsx
+++ components/hero.tsx
@@ -3,10 +3,10 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <main className="lg:px-40 px-4">
-      <div className="grid grid-cols-1 lg:grid-cols-2 mt-20">
-        <div className="">
-          <h1 className="text-4xl lg:text-7xl font-bold">
+    <main className="lg:px-40 px-4 bg-[#E8F5E9] min-h-screen">
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 lg:gap-2">
+        <div className="mt-20">
+          <h1 className="text-4xl lg:text-7xl font-semibold">
             Empowering individuals to make a positive impact
           </h1>
           <p className="text-slate-600 mt-4">
@@ -19,12 +19,13 @@ const Hero = () => {
             Save the Planet
           </button>
         </div>
-        <div className="flex justify-center items-center lg:ml-20">
+        <div className="flex justify-start items-end lg:ml-20 rounded-3xl">
           <Image
-            src="/images/hero-img.png"
+            src="/images/hero.jpg"
             alt="hero-image"
             width="600"
             height="600"
+            className="rounded-3xl"
           />
         </div>
       </div>
